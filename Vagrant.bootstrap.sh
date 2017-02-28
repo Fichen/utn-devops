a4b7552be59e0872e4a7af27384f569d4c1f54b4
Fichen <davidfichtenbaum@gmail.com>
2017-02-28 11:24:24 -0300

    clone repository from github

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -8,20 +8,35 @@ sudo apt-get updates
 # Instalo un servidor web
 sudo apt-get install -y apache2 
 
-
 ### Configuración del entorno ###
 
 # ruta raíz del servidor web
 APACHE_ROOT="/var/www";
 # ruta de la aplicación
-APP_PATH="$APACHE_ROOT/utn-devops";
+APP_PATH="$APACHE_ROOT/utn-devops-app";
 
 
 ## configuración servidor web
 #copio el archivo de configuración del repositorio en la configuración del servidor web
-sudo mv /tmp/devops.site.conf /etc/apache2/sites-available
-#activo el nuevo sitio web
-sudo a2ensite devops.site.conf
-#refresco el servicio del servidor web para que tome la nueva configuración
-sudo service apache2 reload
+if [ -f "/tmp/devops.site.conf" ]; then
+	echo "Copio el archivo de configuracion de apache";
+	sudo mv /tmp/devops.site.conf /etc/apache2/sites-available
+	#activo el nuevo sitio web
+	sudo a2ensite devops.site.conf
+	#desactivo el default
+	sudo a2dissite 000-default.conf
+	#refresco el servicio del servidor web para que tome la nueva configuración
+	sudo service apache2 reload
+fi
 	
+## aplicación
+
+# descargo la app del repositorio
+if [ ! -d "$APP_PATH" ]; then
+	echo "clono el repositorio";
+	cd $APACHE_ROOT;
+	sudo git clone https://github.com/Fichen/utn-devops-app.git;
+	cd $APP_PATH;
+	sudo git checkout unidad-1;
+fi
+
--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
 
   # Redirecciono puertos desde la maquina virtual a la maquina real. Por ejemplo 
   # del puerto 80 (web) de la maquina virtual con Debian se podrá acceder a través
-  # del puerto 8080 de nuestro navegador.
+  # del puerto 8081 de nuestro navegador.
   # Esto se realiza para poder darle visibilidad a los puertos de la maquina virtual 
   # y además para que no se solapen los puertos con los de nuestra equipo en el caso de que
   # ese número de puerto este en uso.
