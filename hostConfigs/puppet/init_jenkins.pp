e5db28a2293712b1ccd1b33290049504aeb79620
Fichen <davidfichtenbaum@gmail.com>
2017-11-03 00:25:07 -0300

    Added Jenkins dependencies for php

--- hostConfigs/puppet/init_jenkins.pp
+++ hostConfigs/puppet/init_jenkins.pp
@@ -1,4 +1,7 @@
 class jenkins {
+
+	$jenkins_pwd = 'utndevops'
+
     # get key
     exec { 'install_jenkins_key':
         command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add - ',
@@ -52,4 +55,20 @@ class jenkins {
         ensure  => running,
         require => Package['jenkins'],
     }
+	
+	user { 'jenkins':
+	    ensure	=> present,
+	    password => '$1$hrl1RNSP$DoKnhDdeCLlW.QJGLY8dj1' #utndevops
+	}
+	
+	exec { "install jenkins cli & plugins":
+		cwd         => "/tmp",
+		command     => "wget http://127.0.0.1:8082/jnlpJars/jenkins-cli.jar && java -jar jenkins-cli.jar -s http://127.0.0.1:8082 install-plugin checkstyle cloverphp crap4j dry htmlpublisher jdepend plot pmd violations warnings xunit git greenballs --username jenkins --password ${jenkins_pwd}",
+		path    => ['/usr/bin', '/usr/sbin',],
+	}
+	
+	exec { "install php dependencies for jenkins ":
+		command     => "sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php && sudo apt-get update && sudo apt-get install -y php7.0 php7.0-xdebug php7.0-xsl php7.0-dom php7.0-zip php7.0-mbstring",
+		path    => ['/usr/bin', '/usr/sbin',],
+	}
 }
