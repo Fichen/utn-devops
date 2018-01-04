ace327ef56f458db3f4e9f56ead7282110797e25
Fichen <davidfichtenbaum@gmail.com>
2018-01-03 21:30:19 -0300

    Transferred changes from branch unidad-3 to unidad-4

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -79,6 +79,8 @@ sudo mv -f /tmp/init_jenkins.pp /etc/puppet/modules/jenkins/manifests/init.pp
 sudo mv -f /tmp/jenkins_default /etc/puppet/modules/jenkins/files/jenkins_default
 sudo mv -f /tmp/jenkins_init_d /etc/puppet/modules/jenkins/files/jenkins_init_d
 
+sudo dos2unix /etc/puppet/modules/jenkins/files/jenkins_init_d
+
 # al detener e iniciar el servicio se regeneran los certificados 
 sudo service puppetmaster stop && service puppetmaster start
 
--- hostConfigs/puppet/init_jenkins.pp
+++ hostConfigs/puppet/init_jenkins.pp
@@ -5,65 +5,49 @@ class jenkins {
     # get key
     exec { 'install_jenkins_key':
         command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add - ',
-    }
+    } 
 
-    # actualizo los repositorios
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update',
-        require => File['/etc/apt/sources.list.d/jenkins.list','/etc/apt/sources.list.d/ondrej-ubuntu-php-xenial.list'],
-    }
-
-    # archivo que contiene el repositorio para la instalación de Jenkins
+	# source file
     file { '/etc/apt/sources.list.d/jenkins.list':
         content => "deb https://pkg.jenkins.io/debian-stable binary/\n",
+		ensure => present,
         mode    => '0644',
         owner   => root,
         group   => root,
         require => Exec['install_jenkins_key'],
-    }
-	
-	#Archivo que contiene un repositorio para la instalación de paquetes de PHP
-	file { '/etc/apt/sources.list.d/ondrej-ubuntu-php-xenial.list':
-        content => "deb http://ppa.launchpad.net/ondrej/php/ubuntu xenial main\n",
-        mode    => '0644',
-        owner   => root,
-        group   => root,
-    }
-
-    # Instalación de jenkins 
+    } -> #ordeno la secuencia de pasos en el tiempo mediante el operador "->".
+		 # se utiliza para encadenar semanticamente distintas declaraciones	
+    # update
+    exec { 'apt-get update':
+        command => '/usr/bin/apt-get update',
+        #require => File['/etc/apt/sources.list.d/jenkins.list'],
+    } ->
+    # jenkins package
     package { 'jenkins':
-        ensure  => present,
-        require => Exec['apt-get update'],
-    }
+        ensure  => installed,
+        require => [
+			File['/etc/apt/sources.list.d/jenkins.list'],
+			Exec['apt-get update']
+		]
+    } -> #Reemplazo el puerto de jenkins para que este escuchando en el 8082
+	exec { 'replace_jenkins_port':
+		command => "/bin/sed -i -- 's/HTTP_PORT=8080/HTTP_PORT=8082/g' /etc/default/jenkins",
+		notify => Service['jenkins'],
+	}
 	
-	# Configuración por defecto para jenkins. La diferencia en este archivo 
-	#solo es el cambio de puerto. Del 8080 al 8082
-    file { '/etc/default/jenkins':
-	    ensure  => present,
-		force  => true,
-        mode    => '0644',
-        owner   => root,
-        group   => root,
-        require => Package['jenkins'],
-		source => 'puppet:///modules/jenkins/jenkins_default',
-    }
-	#Archivo para el inicio del servicio de Jenkins. Mismo proposito que el anterior
-	file { '/etc/init.d/jenkins':
-	    ensure  => present,	
-		force  => true,	
-        mode    => '0755',
-        owner   => root,
-        group   => root,
-        require => Package['jenkins'],
-		source => 'puppet:///modules/jenkins/jenkins_init_d',
-    }
-
-    # Aseguro que jenkins siempre se ejecute 
+	# Notifico al gestor de servicios que un archivo cambio
+	exec { 'reload-systemctl':
+        command => '/bin/systemctl daemon-reload',
+    } 
+	
+    # aseguro que el servicio de jenkins este activo
     service { 'jenkins':
         ensure  => running,
-        require => Package['jenkins'],
+	    enable  => "true",
+		require => Exec['reload-systemctl']
     }
 	
+	### Agregado unidad 4
 	# Cambio la clave del usuario de sistema mediante el valor encriptado
 	user { 'jenkins':
 	    ensure	=> present,
@@ -81,12 +65,15 @@ class jenkins {
 	# Instalación de PHP en el equipo que tendrá Jenkins, en este caso de ejemplo es el misma máquina virtual
 	# que contiene toda la práctica.
 	$enhancers = [ 'php7.0', 'php7.0-xdebug', 'php7.0-xsl', 'php7.0-dom', 'php7.0-zip', 'php7.0-mbstring','phpunit', 'php_codesniffer', 'phploc','pdepend','phpmd','phpcpd','php-codebrowse','phpdox']
-	package { $enhancers: ensure => 'installed' }
+	package { $enhancers: 
+		ensure => installed,
+		require => Exec['apt-get update'],
+	}
 
 	#Instalación del aprovisionamiento de paquetes composer de PHP
 	exec { "install_php_composer":
 		cwd         => "/tmp",
 		command     => "php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\" && sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer && rm -f /tmp/composer-setup.php && sudo chown -R jenkins:jenkins /var/lib/jenkins/.composer",
 		path    => ['/usr/bin', '/usr/sbin', '/bin'],
-	}	
+	}		
 }
