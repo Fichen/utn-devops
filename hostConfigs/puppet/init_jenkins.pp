bb6b27707ef5defa846c511f5b1d00fe9e7693ab
Fichen <davidfichtenbaum@gmail.com>
2018-08-20 16:21:06 -0300

    Change way of install package jre and jenkins in puppet

--- hostConfigs/puppet/init_jenkins.pp
+++ hostConfigs/puppet/init_jenkins.pp
@@ -1,43 +1,47 @@
 class jenkins {
+
     # get key
     exec { 'install_jenkins_key':
         command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -',
-    } 
+    }
 
-	# source file
+        # source file
     file { '/etc/apt/sources.list.d/jenkins.list':
         content => "deb https://pkg.jenkins.io/debian-stable binary/\n",
-		ensure => present,
+                ensure => present,
         mode    => '0644',
         owner   => root,
         group   => root,
         require => Exec['install_jenkins_key'],
     } -> #ordeno la secuencia de pasos en el tiempo mediante el operador "->".
-		 # se utiliza para encadenar semanticamente distintas declaraciones	
+                 # se utiliza para encadenar semanticamente distintas declaraciones
     # update
     exec { 'apt-get update':
         command => '/usr/bin/apt-get update',
         #require => File['/etc/apt/sources.list.d/jenkins.list'],
+    }
+
+    #install jenkins
+    $enhancers = [ 'openjdk-8-jre', 'jenkins' ]
+
+    package { $enhancers:
+        ensure => 'installed',
     } ->
-	
-    # jenkins package
-	exec { 'install_apt_jenkins':
-        command => '/usr/bin/apt-get install openjdk-8-jre jenkins',
-    } -> #Reemplazo el puerto de jenkins para que este escuchando en el 8082
-	exec { 'replace_jenkins_port':
-		command => "/bin/sed -i -- 's/HTTP_PORT=8080/HTTP_PORT=8082/g' /etc/default/jenkins",
-		notify => Service['jenkins'],
-	}
-	
-	# Notifico al gestor de servicios que un archivo cambio
-	exec { 'reload-systemctl':
+    #Reemplazo el puerto de jenkins para que este escuchando en el 8082
+    exec { 'replace_jenkins_port':
+        command => "/bin/sed -i -- 's/HTTP_PORT=8080/HTTP_PORT=8082/g' /etc/default/jenkins",
+        notify => Service['jenkins'],
+    }
+
+    # Notifico al gestor de servicios que un archivo cambio
+    exec { 'reload-systemctl':
         command => '/bin/systemctl daemon-reload',
-    } 
-	
+    }
+
     # aseguro que el servicio de jenkins este activo
     service { 'jenkins':
         ensure  => running,
-	    enable  => "true",
-		require => Exec['reload-systemctl']
+            enable  => "true",
+                require => Exec['reload-systemctl']
     }
 }
