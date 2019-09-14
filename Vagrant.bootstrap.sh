81bf3fafa9173c066be02b64062726ab0e0fc8b6
David Fichtenbaum <davidfichtenbaum@gmail.com>
2019-09-13 21:44:31 -0300

    Update Vagrant.bootstrap.sh

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -19,10 +19,18 @@ sudo mkdir -p /var/db/mysql
 sudo mv -f /tmp/ufw /etc/default/ufw
 
 ##Swap
-sudo swapon /swapdir/swapfile
-echo "/swapdir/swapfile       none    swap    sw      0       0" | sudo tee -a /etc/fstab /etc/fstab
-sudo sysctl vm.swappiness=10
-echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
+##Genero una partición swap. Previene errores de falta de memoria
+if [ ! -f "/swapdir/swapfile" ]; then
+	sudo mkdir /swapdir
+	cd /swapdir
+	sudo dd if=/dev/zero of=/swapdir/swapfile bs=1024 count=2000000
+	sudo mkswap -f  /swapdir/swapfile
+	sudo chmod 600 /swapdir/swapfile
+	sudo swapon swapfile
+	echo "/swapdir/swapfile       none    swap    sw      0       0" | sudo tee -a /etc/fstab /etc/fstab
+	sudo sysctl vm.swappiness=10
+	echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
+fi
 
 
 # Configuración applicación
