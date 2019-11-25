85660f1e3044cc9769d19dd199acaab774ad5f9d
Fichen <davidfichtenbaum@gmail.com>
2019-11-24 21:57:31 -0300

    Vagrant enhancement

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -1,89 +1,89 @@
 #!/bin/bash
 
 #Actualizo los paquetes de la maquina virtual
-sudo apt-get update -y ;
+sudo apt-get update -y
 
 #Aprovisionamiento de software
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common dos2unix linux-image-extra-$(uname -r) linux-image-extra-virtual
 
-# Directorio para los archivos de la base de datos MySQL. El servidor de la base de datos 
+# Directorio para los archivos de la base de datos MySQL. El servidor de la base de datos
 # es instalado mediante una imagen de Docker. Esto está definido en el archivo
 # docker-compose.yml
-sudo mkdir -p /var/db/mysql
-
-# Directorio para scripts
-#sudo mkdir /scripts
-# Muevo scripts para el inicio y la detención de los contenedores de Docker
-#sudo mv -f /tmp/docker-stop.sh /scripts
-#sudo mv -f /tmp/docker-start.sh /scripts
-#sudo chmod 755 /scripts/*
-#sudo dos2unix /scripts/docker-start.sh
-#sudo dos2unix /scripts/docker-stop.sh
+if [ ! -d "/var/db/mysql" ]; then
+	sudo mkdir -p /var/db/mysql
+fi
 
 # Muevo el archivo de configuración de firewall al lugar correspondiente
-sudo mv -f /tmp/ufw /etc/default/ufw
+if [ -f "/tmp/ufw" ]; then
+	sudo mv -f /tmp/ufw /etc/default/ufw
+fi
+
 # Muevo el archivo hosts. En este archivo esta asociado el nombre de dominio con una dirección
 # ip para que funcione las configuraciones de Puppet
-sudo mv -f /tmp/etc_hosts.txt /etc/hosts
+if [ -f "/tmp/etc_hosts.txt" ]; then
+	sudo mv -f /tmp/etc_hosts.txt /etc/hosts
+fi
 
 ## Configuración applicación
-# ruta raíz 
-APP_ROOT="/var/www";
+# ruta raíz
+APP_ROOT="/var/www"
 #ruta aplicación
-APP_PATH=$APP_PATH . "/utn-devops-app";
+APP_PATH=$APP_PATH . "/utn-devops-app"
 
-sudo mkdir $APP_ROOT;
+sudo mkdir $APP_ROOT
 # descargo la app del repositorio
-cd $APP_ROOT;
-sudo git clone https://github.com/Fichen/utn-devops-app.git;
-cd $APP_PATH;
-sudo git checkout unidad-2;
+cd $APP_ROOT
+sudo git clone https://github.com/Fichen/utn-devops-app.git
+cd $APP_PATH
+sudo git checkout unidad-2
 
 
 ###### Instalación de Puppet ######
 #configuración de repositorio
-wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
-sudo dpkg -i puppet5-release-xenial.deb
-sudo apt update
+if [ ! -x "$(command -v puppet)" ]; then
+    wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
+    sudo dpkg -i puppet5-release-xenial.deb
+    sudo apt update
 
 
-# Instalación de master
-sudo apt-get install -y puppet-lint puppetmaster
+    # Instalación de master
+    sudo apt-get install -y puppet-lint puppetmaster
 
-# Instalación de agente. Esto se debiera hacer en otro equipo pero se realiza aquí para simplificar
-# el ejemplo
-sudo apt-get install -y puppet 
+    # Instalación de agente. Esto se debiera hacer en otro equipo pero se realiza aquí para simplificar
+    # el ejemplo
+    sudo apt-get install -y puppet
 
-# Muevo el archivo de configuración de Puppet al lugar correspondiente
-sudo mv -f /tmp/puppet-master.conf /etc/puppet/puppet.conf
+    # Muevo el archivo de configuración de Puppet al lugar correspondiente
+    sudo mv -f /tmp/puppet-master.conf /etc/puppet/puppet.conf
 
-# elimino certificados de que se generan en la instalación.
-# no nos sirven ya que el certificado depende del nombre que se asigne al maestro
-# y en este ejemplo se modifico.
-sudo rm -rf /var/lib/puppet/ssl
+    # elimino certificados de que se generan en la instalación.
+    # no nos sirven ya que el certificado depende del nombre que se asigne al maestro
+    # y en este ejemplo se modifico.
+    sudo rm -rf /var/lib/puppet/ssl
 
-# Agrego el usuario puppet al grupo de sudo, para no necesitar password al reiniciar un servicio
-sudo usermod -a -G sudo,puppet puppet
+    # Agrego el usuario puppet al grupo de sudo, para no necesitar password al reiniciar un servicio
+    sudo usermod -a -G sudo,puppet puppet
 
-# Estructura de directorios para crear el modulo de Puppet para Docker
-sudo mkdir -p /etc/puppet/modules/docker_install/manifests
-sudo mkdir /etc/puppet/modules/docker_install/files
+    # Estructura de directorios para crear el modulo de Puppet para Docker
+    sudo mkdir -p /etc/puppet/modules/docker_install/manifests
+    sudo mkdir /etc/puppet/modules/docker_install/files
 
-# Lo mismo que el anterior pero para el modulo de Jenkins
-sudo mkdir -p /etc/puppet/modules/jenkins/manifests
-sudo mkdir /etc/puppet/modules/jenkins/files
+    # Lo mismo que el anterior pero para el modulo de Jenkins
+    sudo mkdir -p /etc/puppet/modules/jenkins/manifests
+    sudo mkdir /etc/puppet/modules/jenkins/files
 
-# muevo los archivos que utiliza Puppet
-sudo mv -f /tmp/site.pp /etc/puppet/manifests/
-sudo mv -f /tmp/init.pp /etc/puppet/modules/docker_install/manifests/init.pp
-sudo mv -f /tmp/env /etc/puppet/modules/docker_install/files
-sudo mv -f /tmp/init_jenkins.pp /etc/puppet/modules/jenkins/manifests/init.pp
-sudo mv -f /tmp/jenkins_default /etc/puppet/modules/jenkins/files/jenkins_default
-sudo mv -f /tmp/jenkins_init_d /etc/puppet/modules/jenkins/files/jenkins_init_d
+    # muevo los archivos que utiliza Puppet
+    sudo mv -f /tmp/site.pp /etc/puppet/manifests/
+    sudo mv -f /tmp/init.pp /etc/puppet/modules/docker_install/manifests/init.pp
+    sudo mv -f /tmp/env /etc/puppet/modules/docker_install/files
+    sudo mv -f /tmp/init_jenkins.pp /etc/puppet/modules/jenkins/manifests/init.pp
+    sudo mv -f /tmp/jenkins_default /etc/puppet/modules/jenkins/files/jenkins_default
+    sudo mv -f /tmp/jenkins_init_d /etc/puppet/modules/jenkins/files/jenkins_init_d
 
-sudo dos2unix /etc/puppet/modules/jenkins/files/jenkins_init_d
+    sudo dos2unix /etc/puppet/modules/jenkins/files/jenkins_init_d
+fi
 
-# al detener e iniciar el servicio se regeneran los certificados 
+# al detener e iniciar el servicio se regeneran los certificados
 sudo service puppetmaster stop && service puppetmaster start
 
 # limpieza de configuración del dominio utn-devops.localhost es nuestro nodo agente.
--- Vagrantfile
+++ Vagrantfile
@@ -8,16 +8,16 @@ Vagrant.configure("2") do |config|
   # El directorio completo de boxes se puede ver en la siguiente URL atlas.hashicorp.com/boxes/search
   config.vm.box = "ubuntu/xenial64"
 
-  # Con la siguiente configuración se redireccionan los puertos desde la maquina virtual a la maquina real. 
-  # A diferencia de lo que observamos en la unidad n°1, ahora el servicio que utiliza el puerto en realidad 
+  # Con la siguiente configuración se redireccionan los puertos desde la maquina virtual a la maquina real.
+  # A diferencia de lo que observamos en la unidad n°1, ahora el servicio que utiliza el puerto en realidad
   # va a estar dentro de un contenedor Docker, por lo que el redireccionamiento de los puertos, en este ejemplo,
   # tendran el mismo número. Para aclarar más este concepto, cuando nosotros ingresemos en nuestro navegador la
   # url http://127.0.0.1:8081 la petición irá a la máquina virtual de VirtualBox (aprovisionada mediante Vagrant)
   # y a su vez irá al puerto 8081 que expone el contenedor Docker mediante una redirección de puertos del 80 al 8081
   # Para observar esto último, revise el archivo docker-compose.yml y verá una línea con el contenido "8081:80".
-  # Esto se realiza para poder darle visibilidad a los puertos de la maquina virtual y además para que no se 
+  # Esto se realiza para poder darle visibilidad a los puertos de la maquina virtual y además para que no se
   # solapen los puertos con los de nuestra equipo en el caso de que ese número de puerto este en uso.
-  
+
   config.vm.network "forwarded_port", guest: 8081, host: 8081
   config.vm.network "forwarded_port", guest: 8082, host: 8082
   config.vm.network "forwarded_port", guest: 4400, host: 4400
@@ -34,7 +34,7 @@ Vagrant.configure("2") do |config|
   # config.vm.network "public_network"
 
 
-  # configuración del nombre de maquina 
+  # configuración del nombre de maquina
   config.vm.hostname = "utn-devops.localhost"
   config.vm.provider "virtualbox" do |v|
 	v.name = "utn-devops-vagrant-ubuntu"
@@ -44,7 +44,7 @@ Vagrant.configure("2") do |config|
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
   # config.vm.synced_folder "../data", "/vagrant_data"
-  # 
+  #
   # Mapeo de directorios que se comparten entre la maquina virtual y nuestro equipo. En este caso es
   # el propio directorio donde está el archivo  y el directorio "/vagrant" dentro de la maquina virtual.
   config.vm.synced_folder ".", "/vagrant"
@@ -58,7 +58,7 @@ Vagrant.configure("2") do |config|
   #   # Display the VirtualBox GUI when booting the machine
   #   vb.gui = true
   #
-  #   
+  #
   # Customize the amount of memory on the VM:
     vb.memory = "1024"
   end
@@ -80,10 +80,10 @@ Vagrant.configure("2") do |config|
   #  echo "I am provisioning..."
   #  date > /etc/vagrant_provisioned_at
   #SHELL
-  
+
   # Con esta sentencia lo que hara Vagrant es copiar el archivo a la máquina Ubuntu.
   # Además de usarlo como ejemplo para distinguir dos maneras de aprovisionamiento el archivo contiene
-  # una definición del firewall de Ubuntu para permitir el tráfico de red que se redirecciona internamente, configuración 
+  # una definición del firewall de Ubuntu para permitir el tráfico de red que se redirecciona internamente, configuración
   # necesaria para Docker. Luego será copiado al lugar correcto por el script Vagrant.bootstrap.sh
   config.vm.provision "file", source: "hostConfigs/ufw", destination: "/tmp/utw"
   config.vm.provision "file", source: "hostConfigs/etc_hosts.txt", destination: "/tmp/etc_hosts.txt"
@@ -96,14 +96,11 @@ Vagrant.configure("2") do |config|
   # Archivo para Jenkins
   config.vm.provision "file", source: "hostConfigs/jenkins/default_jenkins", destination: "/tmp/jenkins_default"
   config.vm.provision "file", source: "hostConfigs/jenkins/init_d", destination: "/tmp/jenkins_init_d"
-  # Scripts para el servicio de Docker
-  config.vm.provision "file", source: "hostConfigs/docker-start.sh", destination: "/tmp/docker-start.sh"
-  config.vm.provision "file", source: "hostConfigs/docker-stop.sh", destination: "/tmp/docker-stop.sh"
-  
+
   # Con esta sentencia lo que hara Vagrant es transferir este archivo a la máquina Ubuntu
   # y ejecutarlo una vez iniciado. En este caso ahora tendrá el aprovisionamiento para la instalación de Docker
   config.vm.provision :shell, path: "Vagrant.bootstrap.sh"
 
-  # 
-  
+  #
+
 end
