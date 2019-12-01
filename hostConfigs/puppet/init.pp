df5e3a6a65856faec289cbebbbe4ad742cd558f5
Fichen <davidfichtenbaum@gmail.com>
2019-12-01 12:54:35 -0300

    Change repository from xenial to bionic

--- hostConfigs/puppet/init.pp
+++ hostConfigs/puppet/init.pp
@@ -8,25 +8,25 @@ class docker_install {
 
 
 # Agrego el repositorio para la instalación de Docker
-exec { 'agrego-repositorio':                    
-  command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'  
+exec { 'agrego-repositorio':
+  command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
 } ->
 # Actualización de repositorio. La declaracion de un bloque exec permite definir
 # comandos que ejecutara el nodo cliente de Puppet
-exec { 'apt-update':                    
-  command => '/usr/bin/apt-get update'  
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
 }
 
 # Instalación del paquete docker. Tambien es para ejemplicar que se puede declarar
 # como requisito que se ejecuten una serie de comandos antes de la instalación
 package { 'docker-ce':
-  require => Exec['apt-update','agrego-repositorio','apt-update'],       
+  require => Exec['apt-update','agrego-repositorio','apt-update'],
   ensure => installed,
 }
 
 # Instalación del paquete docker-compose
 package { 'docker-compose':
-  require => Exec['apt-update'],        
+  require => Exec['apt-update'],
   ensure => installed,
 }
 
@@ -36,7 +36,7 @@ file { "/var/www/utn-devops-app/myapp/.env":
   mode => "0644",
     owner => 'root',
     group => 'root',
-  ensure => 'present', 
+  ensure => 'present',
     source => 'puppet:///modules/docker_install/env',
 }
 
@@ -46,10 +46,10 @@ service { 'docker':
 }
 
 exec { 'config-app':
-  command => 'docker-compose -f /vagrant/docker/docker-compose.yml up -d',       
-  path => '/usr/bin',  
+  command => 'docker-compose -f /vagrant/docker/docker-compose.yml up -d',
+  path => '/usr/bin',
   onlyif => 'test $(docker ps |grep apache)',
 }
 
- 
+
 }
