93daf5bd5e90cb780dcdfc376c3f92dd8a7c8ac7
Fichen <davidfichtenbaum@gmail.com>
2022-11-28 16:38:48 -0300

    Upgrade to Java version 11

--- hostConfigs/puppet/init_jenkins.pp
+++ hostConfigs/puppet/init_jenkins.pp
@@ -2,32 +2,30 @@ class jenkins {
 
     # get key
     exec { 'install_jenkins_key':
-        command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -',
+        command => '/usr/bin/curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null',
     }
 
         # source file
     file { '/etc/apt/sources.list.d/jenkins.list':
-        content => "deb https://pkg.jenkins.io/debian-stable binary/\n",
-        ensure => present,
+        content => "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/\n",
         mode    => '0644',
         owner   => root,
         group   => root,
         require => Exec['install_jenkins_key'],
-    } -> #ordeno la secuencia de pasos en el tiempo mediante el operador "->".
+    } #ordeno la secuencia de pasos en el tiempo mediante el operador "->".
     # se utiliza para encadenar semanticamente distintas declaraciones
     # update
-    exec { 'apt-get update':
+    -> exec { 'apt-get update':
         command => '/usr/bin/apt-get update',
     }
 
     #install jenkins
-    $enhancers = [ 'openjdk-8-jre', 'jenkins' ]
+    $enhancers = [ 'openjdk-11-jre', 'jenkins' ]
 
     package { $enhancers:
         ensure => 'installed',
-    } ->
-    #Reemplazo el puerto de jenkins para que este escuchando en el 8082
-    exec { 'replace_jenkins_port':
+    } #Reemplazo el puerto de jenkins para que este escuchando en el 8082
+    -> exec { 'replace_jenkins_port':
         command => "/bin/sed -i -- 's/JENKINS_PORT=8080/JENKINS_PORT=8082/g' /lib/systemd/system/jenkins.service",
     } -> exec { 'reload-systemctl':
         command => '/bin/systemctl daemon-reload',
