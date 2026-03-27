9d7a1dff811f92019327344d0bcf76795e2e96d4
Fichen <davidfichtenbaum@gmail.com>
2026-03-26 22:18:20 -0300

    fixed jenkins install

--- hostConfigs/puppet/init_jenkins.pp
+++ hostConfigs/puppet/init_jenkins.pp
@@ -3,26 +3,21 @@ class jenkins {
 
     # get key
     exec { 'install_jenkins_key':
-        command => '/usr/bin/curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null',
+        command => '/usr/bin/sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key',
     }
 
-    # source file
-    file { '/etc/apt/sources.list.d/jenkins.list':
-        ensure  => present,
-        content => "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/\n",
-        mode    => '0644',
-        owner   => root,
-        group   => root,
-        require => Exec['install_jenkins_key'],
+    -> exec {'add-repository-key':
+        command => 'echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null',
+        path => ['/usr/bin/'],
     }
-
-    # update
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update',
+    -> exec { 'apt-get update':
+        command => '/usr/bin/apt-get update -y',
     }
 
     #install jenkins
-    $enhancers = [ 'openjdk-17-jre', 'jenkins' ]
+    $enhancers = [ 'fontconfig', 'openjdk-21-jre', 'jenkins' ]
 
     # jenkins package
     package { $enhancers:
